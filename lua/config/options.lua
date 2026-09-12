c3a853f20138f77ac43b2a2220ea7e37f5226ae8
Bruno Carpio <bcarpio181@gmail.com>
2026-09-12 19:28:31 -0300

    add xclip clipboard option

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -46,4 +46,16 @@ opt.wildignore:append({ "**/node_modules/*", "**/.git/*" })
 -- Clipboard (sync with system)
 vim.schedule(function()
   opt.clipboard = "unnamedplus"
+
+  vim.g.clipboard = {
+    name = 'xclip',
+    copy = {
+      ['+'] = 'xclip -selection clipboard',
+      ['*'] = 'xclip -selection primary',
+    },
+    paste = {
+      ['+'] = 'xclip -selection clipboard -o',
+      ['*'] = 'xclip -selection primary -o',
+    }
+  }
 end)
