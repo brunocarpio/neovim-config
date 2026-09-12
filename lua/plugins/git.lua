bd70815fcf1a3f8fd15835c2823c51b837272243
Bruno Carpio <bcarpio181@gmail.com>
2026-09-12 19:54:09 -0300

    add gitsigns commands

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,6 +1,5 @@
 -- Git integration
 return {
-  -- Gitsigns (replaces vim-signify)
   {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
@@ -18,24 +17,21 @@ return {
           vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
         end
 
+        local command = function(name, com)
+          vim.api.nvim_create_user_command(name, function()
+            com()
+          end, {})
+        end
+
         -- Navigation
         map("n", "]c", gs.next_hunk, "Next hunk")
         map("n", "[c", gs.prev_hunk, "Previous hunk")
 
-        -- Actions
-        map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
-        map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
-        map("v", "<leader>hs", function() gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Stage hunk")
-        map("v", "<leader>hr", function() gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Reset hunk")
-        map("n", "<leader>hS", gs.stage_buffer, "Stage buffer")
-        map("n", "<leader>hu", gs.undo_stage_hunk, "Undo stage hunk")
-        map("n", "<leader>hR", gs.reset_buffer, "Reset buffer")
-        map("n", "<leader>hp", gs.preview_hunk, "Preview hunk")
-        map("n", "<leader>hb", function() gs.blame_line({ full = true }) end, "Blame line")
-        map("n", "<leader>tb", gs.toggle_current_line_blame, "Toggle line blame")
-        map("n", "<leader>hd", gs.diffthis, "Diff this")
-        map("n", "<leader>hD", function() gs.diffthis("~") end, "Diff this ~")
-        map("n", "<leader>td", gs.toggle_deleted, "Toggle deleted")
+        -- Commands
+        command('HunkPreview', gs.preview_hunk)
+        command('HunkStage', gs.stage_hunk)
+        command('HunkReset', gs.reset_hunk)
+        command('HunkDiffThis', gs.diffthis)
       end,
     },
   },
@@ -43,12 +39,6 @@ return {
   -- Fugitive (Git commands)
   {
     "tpope/vim-fugitive",
-    lazy = false, -- Load immediately so :G and :Git are always available
-    keys = {
-      { "<leader>gs", "<cmd>Git<CR>", desc = "Git status (fugitive)" },
-      { "<leader>gb", "<cmd>Git blame<CR>", desc = "Git blame" },
-      { "<leader>gd", "<cmd>Gdiffsplit<CR>", desc = "Git diff split" },
-      { "<leader>gl", "<cmd>Git log<CR>", desc = "Git log" },
-    },
+    lazy = false,
   },
 }
