b95fc99d6f375ef3fab8a0a547cb04dc96a62180
Bruno Carpio <bcarpio181@gmail.com>
2026-09-12 22:09:58 -0300

    add luasnips

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -69,4 +69,27 @@ return {
       })
     end,
   },
+
+
+  {
+    "L3MON4D3/LuaSnip",
+    version = "v2.*",
+    build = "make install_jsregexp",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+      end,
+    },
+    config = function()
+      local luasnip = require("luasnip")
+
+      luasnip.config.setup({
+        history = true,
+        updateevents = "TextChanged,TextChangedI",
+      })
+      -- Load custom Lua or VSCode-style snippets if needed
+      -- require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/luasnippets" })
+    end,
+  }
 }
